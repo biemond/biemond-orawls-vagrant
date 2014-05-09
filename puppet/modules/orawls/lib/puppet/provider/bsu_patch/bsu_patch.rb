2d17d897011d81f33b1a1609b64a0cf1640d1be0
Edwin Biemond <biemond@gmail.com>
2014-05-09 15:54:00 +0200

    bsu fix

--- puppet/modules/orawls/lib/puppet/provider/bsu_patch/bsu_patch.rb
+++ puppet/modules/orawls/lib/puppet/provider/bsu_patch/bsu_patch.rb
@@ -1,4 +1,3 @@
-require 'rexml/document' 
 
 Puppet::Type.type(:bsu_patch).provide(:bsu_patch) do
 
@@ -44,21 +43,21 @@ Puppet::Type.type(:bsu_patch).provide(:bsu_patch) do
     patch_download_dir  = resource[:patch_download_dir]
 
     if patch_download_dir == nil
-      command = jdk_home_dir+"/bin/java -Xms256m -Xmx256m -jar "+middleware_home_dir+"/utils/bsu/patch-client.jar -report -bea_home="+middleware_home_dir+" -output_format=xml"
+      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh -view -status=applied -prod_dir="+weblogic_home_dir+" -verbose"
     else 
-      command = jdk_home_dir+"/bin/java -Xms256m -Xmx256m -jar "+middleware_home_dir+"/utils/bsu/patch-client.jar -report -bea_home="+middleware_home_dir+" -output_format=xml -patch_download_dir="+patch_download_dir
+      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh -view -status=applied -prod_dir="+weblogic_home_dir+" -patch_download_dir="+patch_download_dir+" -verbose"
     end 
 
     Puppet.debug "bsu_status for patch #{patchName} command: #{command}"
-
     output = Puppet::Util::Execution.execute command, :failonfail => true ,:uid => user
-    doc = REXML::Document.new output
-     
-    root = doc.root
-    root.elements.each("//patchDesc") do |patch|
-      if patch.elements['patchId'].text == patchName
-        Puppet.debug "found patch"
-        return patchName
+
+    output.each_line do |li|
+      unless li.nil?
+        Puppet.debug "line #{li}" 
+        if li.include? patchName
+          Puppet.debug "found patch"
+          return patchName
+        end
       end 
     end
     return "NotFound"
