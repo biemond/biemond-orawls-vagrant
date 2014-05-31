d7cbfd27a9cf9585936b79e42df625226c5d6eaf
Edwin Biemond <biemond@gmail.com>
2014-05-31 13:35:18 +0200

    webtier 12.1.2 support

--- puppet/modules/orawls/manifests/fmw.pp
+++ puppet/modules/orawls/manifests/fmw.pp
@@ -4,12 +4,13 @@
 #
 ##
 define orawls::fmw (
+  $version                    = hiera('wls_version'               , 1111),  # 1036|1111|1211|1212
   $weblogic_home_dir          = hiera('wls_weblogic_home_dir'     , undef), # /opt/oracle/middleware11gR1/wlserver_103
   $middleware_home_dir        = hiera('wls_middleware_home_dir'   , undef), # /opt/oracle/middleware11gR1
   $oracle_base_home_dir       = hiera('wls_oracle_base_home_dir'  , undef), # /opt/oracle
   $oracle_home_dir            = undef,                                      # /opt/oracle/middleware/Oracle_SOA
   $jdk_home_dir               = hiera('wls_jdk_home_dir'          , undef), # /usr/java/jdk1.7.0_45
-  $fmw_product                = undef,                                      # adf|soa|osb
+  $fmw_product                = undef,                                      # adf|soa|osb|wcc|wc|oim|web
   $fmw_file1                  = undef,
   $fmw_file2                  = undef,
   $os_user                    = hiera('wls_os_user'               , undef), # oracle
@@ -21,7 +22,6 @@ define orawls::fmw (
   $temp_directory             = hiera('wls_temp_dir'              ,'/tmp'), # /tmp directory
 )
 {
-
   $exec_path     = "${jdk_home_dir}/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:"
   $oraInventory  = "${oracle_base_home_dir}/oraInventory"
 
@@ -62,6 +62,7 @@ define orawls::fmw (
     else {
       $oracleHome = $oracle_home_dir
     }
+    $createFile1              = "${download_dir}/${fmw_product}/Disk1"
     $total_files              = 1
 
   } elsif ( $fmw_product == "soa" ) {
@@ -72,6 +73,7 @@ define orawls::fmw (
     else {
       $oracleHome = $oracle_home_dir
     }
+    $createFile1              = "${download_dir}/${fmw_product}/Disk1"
     $createFile2              = "${download_dir}/${fmw_product}/Disk4"
     $total_files              = 2
 
@@ -83,6 +85,7 @@ define orawls::fmw (
     else {
       $oracleHome = $oracle_home_dir
     }
+    $createFile1              = "${download_dir}/${fmw_product}/Disk1"
     $total_files              = 1
 
   } elsif ( $fmw_product == "oim" ) {
@@ -93,6 +96,7 @@ define orawls::fmw (
     else {
       $oracleHome = $oracle_home_dir
     }
+    $createFile1              = "${download_dir}/${fmw_product}/Disk1"
     $createFile2              = "${download_dir}/${fmw_product}/Disk4"
     $total_files              = 2
 
@@ -104,6 +108,7 @@ define orawls::fmw (
     else {
       $oracleHome = $oracle_home_dir
     }
+    $createFile1              = "${download_dir}/${fmw_product}/Disk1"
     $total_files              = 1
 
   } elsif ( $fmw_product == "wcc" ) {
@@ -114,24 +119,51 @@ define orawls::fmw (
     else {
       $oracleHome = $oracle_home_dir
     }
+    $createFile1              = "${download_dir}/${fmw_product}/Disk1"
     $createFile2              = "${download_dir}/${fmw_product}/Disk2"
     $total_files              = 2
 
+  } elsif ( $fmw_product == "web" ) {
+
+    if $version == 1212 { 
+      $fmw_silent_response_file = "orawls/web_http_server_1212.rsp.erb"
+    } else {
+      $fmw_silent_response_file = "orawls/web_http_server.rsp.erb"
+    }  
+
+    if ($oracle_home_dir == undef) {
+      if $version == 1212 { 
+        $oracleHome               = "${middleware_home_dir}/ohs"
+      } else {
+        $oracleHome               = "${middleware_home_dir}/WT1"
+      }  
+    }
+    else {
+      $oracleHome = $oracle_home_dir
+    }
+    $binFile1                 = "ohs_121200_linux64.bin"
+    $createFile1              = "${download_dir}/${fmw_product}/${binFile1}"
+    $total_files              = 1
+
   } else {
-    fail('unknown fmw_product value choose adf|soa|osb|oim|wc|wcc')
+    fail('unknown fmw_product value choose adf|soa|osb|oim|wc|wcc|web')
   }
 
-  # check if the oracle home already exists, this is for performance reasons
-  $found = orawls_oracle_exists($oracleHome)
-
-  if $found == undef {
+  # check if the oracle home already exists, only for < 12.1.2, this is for performance reasons
+  if $version == 1212 {
     $continue = true
   } else {
-    if ($found) {
-      $continue = false
-    } else {
-      notify { "orawls::fmw ${title} ${oracleHome} does not exists": }
+    $found = orawls_oracle_exists($oracleHome)
+
+    if $found == undef {
       $continue = true
+    } else {
+      if ($found) {
+        $continue = false
+      } else {
+        notify { "orawls::fmw ${title} ${oracleHome} does not exists": }
+        $continue = true
+      }
     }
   }
 
@@ -176,7 +208,7 @@ define orawls::fmw (
       
     exec { "extract ${fmw_file1}":
       command   => "unzip -o ${disk1_file} -d ${download_dir}/${fmw_product}",
-      creates   => "${download_dir}/${fmw_product}/Disk1",
+      creates   => $createFile1,
       path      => $exec_path,
       user      => $os_user,
       group     => $os_group,
@@ -248,26 +280,42 @@ define orawls::fmw (
     $command = "-silent -response ${download_dir}/${title}_silent_${fmw_product}.rsp -waitforcompletion"
 
     #notify { "orawls::fmw ${download_dir}/${fmw_product}/Disk1/install/${installDir}/runInstaller ${command} -invPtrLoc ${oraInstPath}/oraInst.loc -ignoreSysPrereqs -jreLoc ${jdk_home_dir} -Djava.io.tmpdir=${temp_directory}": } 
+    
+    if $version == 1212 {
+      exec { "install ${fmw_product} ${title}":
+        command     => "${download_dir}/${fmw_product}/${binFile1} ${command} -invPtrLoc ${oraInstPath}/oraInst.loc -ignoreSysPrereqs -jreLoc ${jdk_home_dir} -Djava.io.tmpdir=${temp_directory}",
+        environment => "TMP=${temp_directory}",
+        timeout     => 0,
+        creates     => $oracleHome,
+        path        => $exec_path,
+        user        => $os_user,
+        group       => $os_group,
+        logoutput   => $log_output,
+        require     => [File["${download_dir}/${title}_silent_${fmw_product}.rsp"],
+                        Orawls::Utils::Orainst["create oraInst for ${fmw_product}"],
+                        Exec["extract ${fmw_file1}"],],
+      }
+    } else {
+      file { $oracleHome:
+        ensure => "directory",
+        owner  => $os_user,
+        group  => $os_group,
+        before => Exec["install ${fmw_product} ${title}"],      
+      }
 
-    file { $oracleHome:
-      ensure => "directory",
-      owner  => $os_user,
-      group  => $os_group,
-      before => Exec["install ${fmw_product} ${title}"],      
-    }
-
-    exec { "install ${fmw_product} ${title}":
-      command     => "${download_dir}/${fmw_product}/Disk1/install/${installDir}/runInstaller ${command} -invPtrLoc ${oraInstPath}/oraInst.loc -ignoreSysPrereqs -jreLoc ${jdk_home_dir} -Djava.io.tmpdir=${temp_directory}",
-      environment => "TMP=${temp_directory}",
-      timeout     => 0,
-      creates     => "${oracleHome}/OPatch",
-      path        => $exec_path,
-      user        => $os_user,
-      group       => $os_group,
-      logoutput   => $log_output,
-      require     => [File["${download_dir}/${title}_silent_${fmw_product}.rsp"],
-                      Orawls::Utils::Orainst["create oraInst for ${fmw_product}"],
-                      Exec["extract ${fmw_file1}"],],
-    }
+      exec { "install ${fmw_product} ${title}":
+        command     => "${download_dir}/${fmw_product}/Disk1/install/${installDir}/runInstaller ${command} -invPtrLoc ${oraInstPath}/oraInst.loc -ignoreSysPrereqs -jreLoc ${jdk_home_dir} -Djava.io.tmpdir=${temp_directory}",
+        environment => "TMP=${temp_directory}",
+        timeout     => 0,
+        creates     => "${oracleHome}/OPatch",
+        path        => $exec_path,
+        user        => $os_user,
+        group       => $os_group,
+        logoutput   => $log_output,
+        require     => [File["${download_dir}/${title}_silent_${fmw_product}.rsp"],
+                        Orawls::Utils::Orainst["create oraInst for ${fmw_product}"],
+                        Exec["extract ${fmw_file1}"],],
+      }
+    }  
   }
 }
