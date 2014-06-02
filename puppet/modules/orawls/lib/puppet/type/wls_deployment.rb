0822d105526515fec65bf094b6f87981e18a8898
Edwin Biemond <biemond@gmail.com>
2014-06-02 22:29:15 +0200

    wls_deployment type

--- puppet/modules/orawls/lib/puppet/type/wls_deployment.rb
+++ puppet/modules/orawls/lib/puppet/type/wls_deployment.rb
@@ -61,9 +61,9 @@ module Puppet
         [
           /^((.*\/)?(.*)?)$/,
           [
-            [ :name        , name     ],
-            [ :domain      , optional ],
-            [ :cluster_name, identity ]
+            [ :name           , name     ],
+            [ :domain         , optional ],
+            [ :deployment_name, identity ]
           ]
         ],
         [
@@ -77,8 +77,15 @@ module Puppet
 
     parameter :name
     parameter :domain
-    property  :source
-    property  :status
-    property  :target 
+    parameter :deployment_name
+    parameter :localpath
+    property  :target
+    property  :targettype 
+    property  :moduletype
+    property  :deploymenttype
+    property  :applicationname
+    property  :versionidentifier
+
+
   end
 end
